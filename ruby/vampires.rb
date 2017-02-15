5efd3f9f8e8e09ef4768bb69ee996a11b286d628
Mina Melosh <minamelosh@gmail.com>
2017-02-15 07:59:28 -0800

    Add Vampire Survey

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you?"
+age = gets.chomp
+puts "Where were you born?"
+origin = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp
