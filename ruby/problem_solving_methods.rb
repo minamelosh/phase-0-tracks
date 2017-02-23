e4b85bbef2e80abe798b073276117d0ee0802c85
Mina Melosh <minamelosh@gmail.com>
2017-02-22 16:28:30 -0800

    Wrote Fib method

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -20,3 +20,47 @@ arr = [42, 89, 23, 1]
 p search_array(arr, 1)
 
 p search_array(arr, 24)
+
+
+#Add the previous two numbers
+def fib(num)
+
+  new_array = []
+
+  count = 1
+  num1 = 0
+  num2 = 1
+  sum1 = 0
+  new_array = [0,1]
+
+  if num == 0
+      new_array = [0]
+      #p "hi"
+  count += 1
+  else
+
+    while new_array.length < num
+
+      if num == 1
+        new_array = [0,1]
+        #p num2
+      else
+        sum1 = num1 + num2
+        new_array.push(sum1)
+        num1 = num2
+        num2 = sum1
+        #p new_array
+      end
+
+    count += 1
+
+    end
+  end
+
+  #p new_array[-1] == 218922995834555169026
+  #p new_array.length
+  #p new_array.index(218922995834555169026)
+  return new_array
+end
+
+p fib(100)
