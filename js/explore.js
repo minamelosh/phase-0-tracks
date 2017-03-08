2511ec269199515235fe421efe186618d89b0724
Mina Melosh <minamelosh@gmail.com>
2017-03-07 23:11:28 -0800

    Put two solutions in file explore.js

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,4 @@
+//creates string of the word in reverse
 function reverse(str){
   var word = "";
   for (var i = str.length -1; i > -1; i -= 1){
@@ -6,5 +7,16 @@ function reverse(str){
   console.log(word);
 }
 
+//fills an array with letters from the original string in the reverse order.
+function backwords(str){
+  var word = [];
+  for (var i = str.length -1; i > -1; i -= 1){
+    word.push(str[i]);
+  }
+  console.log(word);
+}
+
+
 reverse("hello");
+backwords("hello");
 1==1;
