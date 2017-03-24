b60e683d8bf1b708773cc1cb61eaf348d45de673
Mina Melosh <minamelosh@gmail.com>
2017-03-24 10:54:53 -0700

    9.5 pairs

--- web_dev/sinatra_templating/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/sinatra_templating/app.rb
@@ -24,4 +24,25 @@ post '/students' do
   redirect '/'
 end
 
+
 # add static resources
+
+#Module 9.5
+#show student names and campus
+get '/student_campus' do 
+  @campuses = db.execute("SELECT * FROM campuses")
+  erb :template
+end 
+
+get '/new_campus' do 
+  erb :new_campus
+end 
+
+post '/campuses' do
+  p params['name']
+  db.execute("INSERT INTO campuses (name) VALUES (?)", [params['name']])
+  redirect '/student_campus'
+end
+
+
+
